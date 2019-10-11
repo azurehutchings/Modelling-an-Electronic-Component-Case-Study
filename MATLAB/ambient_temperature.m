7f9a98c2f588e92418823ea4c71128056f1fd32a
Jean-Luc <jdanoy3@gmail.com>
2019-10-11 12:37:32 +1000

    ambient temperature

---
+++ MATLAB/ambient_temperature.m
@@ -0,0 +1,11 @@
+%Ambient Temperature
+
+for ambient_temp = 14:0.5:19
+   %Use ambient_temperature variable to reconstruct A matrix and b vector accordingly
+   
+   %Check temperature at point (0.03, 0.03)
+        %If it is close to 50 or 55 celcius return the temperature and
+        %ambient temperature
+        
+        %Narrow down search for ambient temperature
+end
