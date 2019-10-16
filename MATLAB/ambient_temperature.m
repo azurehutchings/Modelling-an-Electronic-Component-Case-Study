344012db9f351b8ca0e67de93b6aa4d82f63e325
Faris503 <54519992+Faris503@users.noreply.github.com>
2019-10-16 12:58:08 +1000

    Effect of ambient temperature
    ambient_temperature.m calculates the temperature at point (0.03, 0.03) based off the ambient temperature.
    
    ambient_temperature_matrix.m creates a new A matrix and b vector from the ambient temperature

--- MATLAB/ambient_temperature.m
+++ MATLAB/ambient_temperature.m
@@ -1,11 +1,24 @@
-%Ambient Temperature
+for ambient_temp = 10:0.5:50
+    
+    [A, b] = ambient_temperature_matrix(ambient_temp);
+    
+    A = cholesky(A);
+    temperatures = forward_substitution(A', b);
+    temperatures = backward_substitution(A, temperatures);
+    
 
-for ambient_temp = 14:0.5:19
    %Use ambient_temperature variable to reconstruct A matrix and b vector accordingly
-   
+
+   temperatures(22)
+
    %Check temperature at point (0.03, 0.03)
+
         %If it is close to 50 or 55 celcius return the temperature and
+
         %ambient temperature
+
         
+
         %Narrow down search for ambient temperature
+
 end
