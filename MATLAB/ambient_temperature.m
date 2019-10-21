74727b8dbde558aef15ce43203bdf49215762f91
Jean-Luc <jdanoy3@gmail.com>
2019-10-21 20:32:02 +1000

    Updated report, case study, ambient temperature
    Case study now uses word "solution" rather than "storage"

--- MATLAB/ambient_temperature.m
+++ MATLAB/ambient_temperature.m
@@ -1,5 +1,5 @@
-
-for ambient_temp = 0:0.00001:2
+format long
+for ambient_temp = 0:0.01:60
     
     [A,b] = full_storage;
     b(33) = (0.2*sqrt(2)/3)*ambient_temp;
@@ -10,7 +10,28 @@ for ambient_temp = 0:0.00001:2
     [temperatures,~] = backward_substitution(cholmat, temperatures, 0);
     
    %Use ambient_temperature variable to reconstruct A matrix and b vector accordingly
-   tol = 1e-6;
+   tol = 1e-3;
+   if abs(temperatures(22)-50) < tol
+      ambient_temp, temperatures(22)
+   end
+   if abs(temperatures(22)-55) < tol
+      ambient_temp, temperatures(22)
+   end
+end
+%%
+
+for ambient_temp = 1.7:0.0000001:1.73
+    
+    [A,b] = full_storage;
+    b(33) = (0.2*sqrt(2)/3)*ambient_temp;
+    b(29) = (0.2*sqrt(2)/3)*ambient_temp;
+    b(24) = (0.2*sqrt(2)/3)*ambient_temp;
+    [cholmat,~] = cholesky(A, 0);
+    [temperatures,~] = forward_substitution(cholmat', b, 0);
+    [temperatures,~] = backward_substitution(cholmat, temperatures, 0);
+    
+   %Use ambient_temperature variable to reconstruct A matrix and b vector accordingly
+   tol = 1e-8;
    if abs(temperatures(22)-50) < tol
       ambient_temp, temperatures(22)
    end
