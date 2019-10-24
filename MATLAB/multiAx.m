bc806da5ffdf01aefe31b7025245cc45cadc3cbc
azurehutchings <azurehutchings@gmail.com>
2019-10-24 15:19:07 +1000

    More changes to CSR in Report

--- MATLAB/multiAx.m
+++ MATLAB/multiAx.m
@@ -3,9 +3,11 @@ function [A_x, flops] = multiAx(v, rb, c, x, current_flops)
 n = length(rb) - 1;
 A_x = zeros(n,1);
 
+%flop count
 flops = current_flops;
 
 for i = 1:n
+    %A * x
     A_x(i) = v(rb(i):(rb(i + 1) - 1)) * x(c(rb(i):(rb(i + 1) - 1)));
-    flops = flops + 1;
+    flops = flops + 1; %flop count
 end
